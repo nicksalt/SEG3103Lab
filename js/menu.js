c5a2379a86643d65f932d5c78d0279f8a3b5b8f0
Nick Salt <nicksalt24@gmail.com>
2019-06-03 19:35:06 -0400

    remove menu items

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 $(function(){
-
-  $(".menu-item").click(function(){ addToCart($(this)); });
-
+    $(".menu-item").click(function(){ addToCart($(this)); });
+    $(".cart-items").on('click', '.cart-item', function(e){removeFromCart($(this), e)})
+    
 });
 
 function addToCart(menuItem) {
@@ -10,10 +10,17 @@ function addToCart(menuItem) {
                 <div class="cart-food col-md-6"><h5>${menuItem.find('.meal').text()}</h5></div>
                 <div class="cart-price col-md-3"><h5>${menuItem.find('.price').text()}</h5></div>
                 <div class="cart-icon col-md-3">
-                    <button type="button" class="btn btn-default remove-item">
+                    <button type="button remove-cart" class="btn btn-default remove-item">
                         <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
                     </button>
                 </div>
             </div>`
     $(".cart-items").append(item);
+}
+
+function removeFromCart(removeButton,e) {
+    var classList = e.target.classList;
+    if (classList.contains("fa-trash") || classList.contains("remove-item")){
+        removeButton.remove();
+    } 
 }
