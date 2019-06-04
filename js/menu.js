eb1cc578cdc0499f4045041faa56754550fcfabf
Nick Salt <nicksalt24@gmail.com>
2019-06-03 20:05:20 -0400

    Update total

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,15 @@
-$(function(){
+var total = 0;
+$(function(){ 
+    $("#nav-placeholder").load("topnav.html");
     $(".menu-item").click(function(){ addToCart($(this)); });
     $(".cart-items").on('click', '.cart-item', function(e){removeFromCart($(this), e)})
+    $("#input").click(function(){alert("Not implemented yet.")})
     
 });
 
 function addToCart(menuItem) {
     console.log(menuItem.find('.meal').text() + ", " + menuItem.find('.price').text());
+    total = total + Number(menuItem.find('.price').text().substr(1));
     var item = `<div class="cart-item row align-items-center">
                 <div class="cart-food col-md-6"><h5>${menuItem.find('.meal').text()}</h5></div>
                 <div class="cart-price col-md-3"><h5>${menuItem.find('.price').text()}</h5></div>
@@ -16,11 +20,14 @@ function addToCart(menuItem) {
                 </div>
             </div>`
     $(".cart-items").append(item);
+    $("#total-price").text("$" + total.toFixed(2));
 }
 
 function removeFromCart(removeButton,e) {
     var classList = e.target.classList;
     if (classList.contains("fa-trash") || classList.contains("remove-item")){
+        total = total - removeButton.closest(".cart-item").find('.cart-price').find('h5').text().substr(1);
         removeButton.remove();
+        $("#total-price").text("$" + total.toFixed(2));
     } 
 }
