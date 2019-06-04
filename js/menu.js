6beb05ef42f7010112940f6c58e256973fb5adfe
Nick Salt <nicksalt24@gmail.com>
2019-06-03 20:32:34 -0400

    Added scroll and updated menu items

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,12 @@ $(function(){
     $(".menu-item").click(function(){ addToCart($(this)); });
     $(".cart-items").on('click', '.cart-item', function(e){removeFromCart($(this), e)})
     $("#input").click(function(){alert("Not implemented yet.")})
+    $(".checkout").click(function(){checkout();})
+    $("#menu-categories > li").click(function(){scrollToMenuCategory($(this));})
     
 });
 
 function addToCart(menuItem) {
-    console.log(menuItem.find('.meal').text() + ", " + menuItem.find('.price').text());
     total = total + Number(menuItem.find('.price').text().substr(1));
     var item = `<div class="cart-item row align-items-center">
                 <div class="cart-food col-md-6"><h5>${menuItem.find('.meal').text()}</h5></div>
@@ -28,6 +29,24 @@ function removeFromCart(removeButton,e) {
     if (classList.contains("fa-trash") || classList.contains("remove-item")){
         total = total - removeButton.closest(".cart-item").find('.cart-price').find('h5').text().substr(1);
         removeButton.remove();
+        if (total <= 0) {
+            total = 0;
+        }
         $("#total-price").text("$" + total.toFixed(2));
     } 
+    
+}
+
+function checkout(){
+    if (total == 0){
+        alert("Please add items to your cart before checking out.")
+    } else {
+        window.open('./checkout.html', '_top')
+    }
+}
+
+function scrollToMenuCategory(li) {
+    $('html, body').animate({
+        scrollTop: $("#" + li.text()).offset().top
+    }, 1250);
 }
